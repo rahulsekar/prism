1025012795e9d1aecfeed18b0cbb9c036e811df2
Rahul Sekar <rahul.sekar@gmail.com>
2022-07-06 15:28:55 +0530

    use previous close is optional

--- app/fin/bondfns.py
+++ app/fin/bondfns.py
@@ -8,7 +8,7 @@ from app.fin.bond import Bond
 from app.fin.base import Security
 
 
-def get_gsec_mktdata():
+def get_gsec_mktdata(use_prev_close: bool = False):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
     r1 = requests.get('https://www.nseindia.com', headers = headers)
     resp = requests.get('https://www.nseindia.com/api/liveBonds-traded-on-cm',
@@ -16,7 +16,11 @@ def get_gsec_mktdata():
     # print(resp.status_code)
     raw = resp.json()['data']
     # print(raw[0])
-    filtered = [(x['symbol'], x['faceValue'], x['series'], x['lastPrice'] or x['previousClose']) for x in raw]
+    filtered = [(x['symbol'],
+                 x['faceValue'],
+                 x['series'],
+                 x['lastPrice'] or (x['previousClose'] if use_prev_close else 0)
+                ) for x in raw]
     df = pd.DataFrame(filtered, columns= ['sym', 'ser', 'fv', 'prc'])
     return df
 
--- app/fin/yield_curve.py
+++ app/fin/yield_curve.py
@@ -17,9 +17,13 @@ def generate():
     filtered_gsecs = [gsec for gsec in gsecs if gsec.product.is_price_sane(gsec.price)]
     dc = disc_curve.NelsonSiegel(filtered_gsecs)
 
-    days = [1, 30, 91, 182, 365, 365 * 3, 365 * 5, 365 * 10, 365 * 20]
-    rts = [dc.yld(datetime.date.today() + rd.relativedelta(days=d)) for d in days]
-    plt.plot(days, rts)
+    days = [1, 30, 91, 182, 365, 365 * 3, 365 * 5, 365 * 10, 365 * 20, 365 * 30, 365 * 40]
+    rts = [dc.yld(datetime.date.today() + rd.relativedelta(days=d)) * 100 for d in days]
+    plt.plot([datetime.date.today() + rd.relativedelta(days=d) for d in days], rts)
+    fil_df = df[(0.04 < df.ytm) & (df.ytm < .10)]
+    plt.scatter(fil_df.mat, fil_df.ytm * 100)
+    plt.xlabel('date')
+    plt.ylabel('yield')
     plt.show()
 
 generate()
